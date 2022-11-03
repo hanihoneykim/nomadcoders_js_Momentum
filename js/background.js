dfae04780b14694117781991a96dfbecee879a4b
hanihoneykim <haeehani@gmail.com>
2022-11-03 23:07:06 +0900

    221103 Quotes&Background 수업과 colorBackground 과제 제출

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images =[
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpeg",
+    "img4.jpg",
+    "img5.jpg",
+    "img6.jpg",
+    "img7.png",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`
+document.querySelector("body").style = `background:url('img/${chosenImage}')`;
+
+document.body.appendChild(bgImage);
---
+++ momentum 과제/t_bgColor_app.js
@@ -0,0 +1,32 @@
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+const colorBtn = document.querySelector("#colorBtn");
+
+function btnClick() {
+    const firstColor = colors[Math.floor(Math.random() * colors.length)];
+    const secondColor = colors[Math.floor(Math.random() * colors.length)];
+
+    document.querySelector("body").style = `background:linear-gradient(to left, ${firstColor}, ${secondColor})`;
+}
+
+btnClick();
+colorBtn.addEventListener("click", btnClick);
